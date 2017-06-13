eea75c7207b1164d08d1d0bc6544d7e654576cd5
Larry Laski <larry.laski@gmail.com>
2017-06-12 22:17:07 -0400

    reset project

--- src/index.js
+++ src/index.js
@@ -1,63 +1,15 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import request from 'superagent';
-import SearchBar from './components/SearchBar'
-import GifList from './components/GifList'
-import GifModal from './components/GifModal';
 import './styles/app.css'
 
 class App extends Component {
 
-    constructor(props) {
-      super(props)
-
-       this.state = {
-            gifs: [],
-            selectedGif: null,
-            modalOpen: false
-        }
-    }
-
-    openModal = (gif) => {
-        this.setState({
-            modalOpen: true,
-            selectedGif: gif
-        })
-    }
-
-    closeModal = () => {
-        this.setState({
-            modalOpen: false,
-            selectedGif: null
-        })
-    }
-
     render() {
         return (
             <div>
-                <SearchBar onTermChange={this.handleTermChange} />
-                <GifList
-                    gifs={this.state.gifs}
-                    onGifSelect={this.openModal}
-                />
-                <GifModal
-                    modalOpen={this.state.modalOpen}
-                    selectedGif={this.state.selectedGif}
-                    onRequestClose={this.closeModal}
-                />
+                <p>Hello</p>
             </div>
         )
-    }
-
-    handleTermChange = (term) => {
-        term = term.replace(/\s/g, '+') //url friendly
-        const url = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=dc6zaTOxFJmzC`;
-
-        request.get(url, (err, res) => {
-            this.setState({
-                gifs: res.body.data
-            })
-        });
     }
 
 }
